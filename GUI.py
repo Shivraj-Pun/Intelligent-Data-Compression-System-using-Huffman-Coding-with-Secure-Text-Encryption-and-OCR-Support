528c78f0b72e84da6c7344bc5b1bc592cf7a166e
Shivraj-Pun <118063371+Shivraj-Pun@users.noreply.github.com>
2025-05-16 20:33:01 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, request, send_file
-from encryption_and_image_protection import AESEncryption, ImageProtector
+from flask import Flask, render_template, request, send_file, session, redirect, url_for, flash
+from encryption_and_image_protection import AESEncryption, ImageProtector, Authenticator
 import os
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
 aes = AESEncryption()
-ROOT_KEY = "my-secret-root-key"  # Change this to something more secure
+authenticator = Authenticator()
+BLUR_COORDINATES = [(50, 50, 200, 100)]  # You can make this dynamic later
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
     encrypted_text = ''
     decrypted_text = ''
     if request.method == 'POST':
@@ -21,16 +25,54 @@ def index():
     return render_template('index.html', encrypted_text=encrypted_text, decrypted_text=decrypted_text)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if authenticator.authenticate(username, password):
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('login'))
+
+
 @app.route('/image-blur', methods=['GET', 'POST'])
 def image_blur():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    
     if request.method == 'POST':
-        if 'image' in request.files:
+        if 'image' in request.files and 'password' in request.form:
             image = request.files['image']
-            coordinates = [(50, 50, 200, 100)]  # Can be dynamic as per UI input
-            image.save('uploaded_image.png')
-            ImageProtector.blur_text_in_image('uploaded_image.png', 'blurred_image.png', coordinates)
-            return send_file('blurred_image.png', as_attachment=True)
+            password = request.form.get('password')
+            if authenticator.authenticate('root', password):
+                image.save('uploaded_image.png')
+                ImageProtector.blur_text_in_image('uploaded_image.png', 'blurred_image.png', BLUR_COORDINATES)
+                return send_file('blurred_image.png', as_attachment=True)
+            else:
+                flash('Incorrect password!')
     return render_template('image_blur.html')
+
+
+@app.route('/image-unblur', methods=['GET', 'POST'])
+def image_unblur():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    
+    if request.method == 'POST':
+        password = request.form.get('password')
+        if authenticator.authenticate('root', password):
+            ImageProtector.unblur_text_in_image('blurred_image.png', 'unblurred_image.png', BLUR_COORDINATES, 'uploaded_image.png')
+            return send_file('unblurred_image.png', as_attachment=True)
+        else:
+            flash('Incorrect password!')
+    return render_template('image_unblur.html')
 
 
 if __name__ == '__main__':
